dec62405d67a4b210d27e0fa443664f3ec26a9c8
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2022-05-04 17:19:50 +0900

    build: replace `pnpx` with `npx`

--- scripts/build.sh
+++ scripts/build.sh
@@ -11,11 +11,11 @@ rm -f app/index.css
 pnpm run tailwind
 
 # default "node-cjs" build
-BUILD_MODE=production pnpx remix build
+BUILD_MODE=production npx remix build
 
 # run esbuild again manually to bundle node_modules
 # (skip `mysql` which appears in https://github.com/knex/knex/blob/3616791ac2a6d17d55b29feed6a503a793d7c488/lib/dialects/mysql/index.js#L23)
-pnpx esbuild "$build_dir/index.js" --bundle --platform=node "--outfile=$build_dir/index-bundle.js" --external:mysql
+npx esbuild "$build_dir/index.js" --bundle --platform=node "--outfile=$build_dir/index-bundle.js" --external:mysql
 
 # zip it as a prebuilt netlify function
 mkdir -p "$build_dir/netlify"
